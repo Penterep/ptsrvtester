a6561884ecde698dd2a1ff50056fb9e8da0632a8
Hexonator <standaspacek25@gmail.com>
2026-07-07 03:56:36 +0200

    PT-1270: added help template

--- ptsrvtester/modules/smb.py
+++ ptsrvtester/modules/smb.py
@@ -1,1 +1,42 @@
-# test comment
+from ._base import BaseModule, BaseArgs, Out
+from ptlibs.ptjsonlib import PtJsonLib
+
+class SMBArgs(BaseArgs):
+    def get_help(self):
+        return [
+            {"description": ["SMB Testing Module"]},
+            {"usage": ["test"]},
+            {"usage_example": [
+                "ptsrvtester smb -ts"
+            ]},
+            {"options": ["-h"]},
+        ]
+
+    def add_subparser(self, name: str, subparsers) -> None:
+        examples = "example usage:\nptsrvtester smb -ts"
+        parser = subparsers.add_parser(
+            name,
+            epilog=examples,
+            add_help=True,
+            formatter_class=argparse.RawTextHelpFormatter,
+        )
+        
+        if not isinstance(parser, argparse.ArgumentParser):
+            raise TypeError  # IDE typing
+
+
+
+class SMB(BaseModule):
+    @staticmethod
+    def module_args():
+        return SMBArgs()
+    
+    def __init__(self, args: BaseArgs, ptjsonlib: PtJsonLib):
+        pass
+    
+    def run(self):
+        pass
+    
+    def output(self):
+        pass
+        
